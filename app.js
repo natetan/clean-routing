793adb063135e3b345299dbd49cb39533ccf8b85
Yulong Tan <yulongtan67@gmail.com>
2019-09-03 23:11:02 -0700

    Add inital middleware route for the root

--- app.js
+++ app.js
@@ -2,4 +2,20 @@
  * This is a Scotch tutorial taken from https://scotch.io/tutorials/keeping-api-routing-clean-using-express-routers
  */
 
- const http = require('http');
+ // Constants
+const PORT = process.env.PORT || 5000;
+
+ // Our dependencies 
+const express = require('express');
+const routes = require('./routes');
+
+// Our express app
+const app = express();
+
+// Connect all of our routes to our application
+app.use('/', routes);
+
+// Listen on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
